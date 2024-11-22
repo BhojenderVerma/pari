e427847a35aa143d20d7c1954c781dd737837104
BhojenderVerma <bhojenderverma07@gmail.com>
2024-11-22 17:06:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showCompliment() {
+    const compliment = document.getElementById("compliment");
+    compliment.textContent = "I don't know, but your eyes are just like black holes—when I look at you, their gravitational pull draws me deeper into a world.and that world  I never want to leave.. ✨";
+    compliment.classList.remove("hidden");
+  }
+  
